72c16303597fdd96327016a5764c13030db72b83
Thummawit <zzc0420285@celestica.com>
2021-09-10 17:54:08 +0700

    Change project name

---
+++ Test1/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Test1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test1.DataContext
+{
+    class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost; Initial Catalog=BrandNew;Integrated Security=True");
+        }
+        public DbSet<Accounts> tbAccounts { get; set; }
+    }
+}
---
+++ Test1/Models/Accounts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace Test1.Models
+{
+    public class Accounts
+    {
+        [Key]
+        public Int64 id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string iban { get; set; }
+        public decimal balance { get; set; }
+    }
+}
